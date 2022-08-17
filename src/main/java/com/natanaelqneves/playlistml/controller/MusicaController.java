f0a011b4adbc9aa638e8626bba53db8e59fb8649
Natanael Queiroz das Neves <natanaelqueiroz@gmail.com>
2022-08-17 13:41:21 -0300

    Update Entities for the project

--- src/main/java/com/natanaelqneves/playlistml/controller/MusicaController.java
+++ src/main/java/com/natanaelqneves/playlistml/controller/MusicaController.java
@@ -3,6 +3,8 @@ package com.natanaelqneves.playlistml.controller;
 import com.natanaelqneves.playlistml.model.Musica;
 import com.natanaelqneves.playlistml.repository.MusicaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +14,7 @@ public class MusicaController {
     @Autowired
     private MusicaRepository musicaRepository;
 
-    @PostMapping
+    @PostMapping("/add")
     public Musica newMusic(Musica musica){
         musicaRepository.save(musica);
         return musica;
@@ -26,6 +28,15 @@ public class MusicaController {
     @GetMapping(path = "/{nome}")
     public Iterable<Musica> getMusicasForName(@PathVariable String nome) {
         return musicaRepository.findByNomeIsContaining(nome);
+    }
+
+    @GetMapping(path = "/{numberPage}/{numberItens}")
+    public Iterable<Musica> getMusicasPorPagina(
+            @PathVariable int numberPage, @PathVariable int numberItens) {
+        if(numberItens >= 25) numberItens = 25;
+
+        Pageable page = PageRequest.of(numberPage, numberItens);
+        return musicaRepository.findAll(page);
     }
 
     @DeleteMapping(path = "/{id}")//SOFT DELETE
